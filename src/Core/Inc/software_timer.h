7c96079beaf8e5f7196fe55a8afa3d6921a4006d
khoi-5 <tranminhkhoichaua5@gmail.com>
2025-09-22 16:18:19 +0700

    ex1

--- src/Core/Inc/software_timer.h
+++ src/Core/Inc/software_timer.h
@@ -8,23 +8,13 @@
 #ifndef INC_SOFTWARE_TIMER_H_
 #define INC_SOFTWARE_TIMER_H_
 
-int timer_flag[10];
-int timer_counter[10];
-int timer_cycle = 10;
-void setTimer(int index, int counter) {
-	timer_flag[index] = 0;
-	timer_counter[index] = counter / timer_cycle;
-}
-void timerRun() {
-	for (int i = 0; i < 10; i++) {
-		if (timer_counter[i] > 0) {
-			timer_counter[i]--;
-		}
-		if (timer_counter[i] <= 0) {
-			timer_flag[i] = 1;
-		}
-	}
-}
+#define MAX_VALUE 10
+
+extern volatile int timer_flag[MAX_VALUE];
+extern volatile int timer_counter[MAX_VALUE];
+extern int timer_cycle;
+void setTimer(int index, int counter);
+void timerRun();
 
 
 #endif /* INC_SOFTWARE_TIMER_H_ */
