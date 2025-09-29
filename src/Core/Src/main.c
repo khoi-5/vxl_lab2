d0c24bd031a5c4a790b44acfba0b850e2e21ef41
khoi-5 <tranminhkhoichaua5@gmail.com>
2025-09-29 10:47:07 +0700

    EX2.1

--- src/Core/Src/main.c
+++ src/Core/Src/main.c
@@ -131,35 +131,9 @@ int main(void)
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-  setTimer(0, 500);
-  HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 0);
-  int counter = 0;
+
   while (1){
-	  if (timer_flag[0] == 1) {
-		switch (counter) {
-		case 0:
-			set_state(0, 1, 1, 1);
-			display7SEG(1);
-			break;
-		case 1:
-			set_state(1, 0, 1, 1);
-			display7SEG(2);
-			HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 1);
-			break;
-		case 2:
-			set_state(1, 1, 0, 1);
-			display7SEG(3);
-			break;
-		case 3:
-			set_state(1, 1, 1, 0);
-			display7SEG(0);
-			HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 0);
-			counter = -1;
-			break;
-		}
-		counter++;
-		setTimer(0, 500);
-	}
+
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
@@ -289,8 +263,37 @@ static void MX_GPIO_Init(void)
 }
 
 /* USER CODE BEGIN 4 */
+int counter = 50 ;
+int led_counter = 0;
 void HAL_TIM_PeriodElapsedCallback(TIM_HandleTypeDef *htim){
-	timerRun();
+	if (counter > 0){
+		counter--;
+		if (counter <= 0){
+			counter = 50;
+			switch (led_counter) {
+		case 0:
+			set_state(0, 1, 1, 1);
+			display7SEG(1);
+			break;
+		case 1:
+			set_state(1, 0, 1, 1);
+			display7SEG(2);
+			HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 1);
+			break;
+		case 2:
+			set_state(1, 1, 0, 1);
+			display7SEG(3);
+			break;
+		case 3:
+			set_state(1, 1, 1, 0);
+			display7SEG(0);
+			HAL_GPIO_WritePin(DOT_GPIO_Port, DOT_Pin, 0);
+			led_counter = -1;
+			break;
+		}
+		led_counter++;
+		}
+	}
 }
 /* USER CODE END 4 */
 
